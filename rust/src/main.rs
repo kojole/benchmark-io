ec2649d008680bcfd2f7a5397afd38128a530a62
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-02-21 12:54:30 +0900

    rust: wait 5s for setup

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,6 @@
 use std::process;
+use std::thread;
+use std::time::Duration;
 
 extern crate benchmark_io_rust as bench;
 use bench::*;
@@ -7,7 +9,11 @@ fn main() {
     let config = cli::parse_args();
     config.hello();
 
+    println!("Wait 5 seconds for setup.");
+    let do_sleep = thread::spawn(|| thread::sleep(Duration::from_secs(5)));
+
     if let Err(err) = setup(config).and_then(|mut bench| {
+        do_sleep.join().unwrap();
         run(&mut bench)?;
         teardown(&mut bench)
     }) {
