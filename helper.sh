2b6819e7c3fb6adf7450d9194733f4b927fc38d9
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-02-21 17:51:22 +0900

    Add helper.sh

---
+++ helper.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -eu
+program="$0"
+
+usage() {
+  cat <<EOF
+Run benchmark-io while mesuring system performance with sysstat.
+
+Usage:
+  $program <bin> <arg>...
+  $program -h | --help
+
+Arguments:
+  bin  path to benchmark-io binary
+  arg  arguments passed to benchmark-io
+
+EOF
+}
+
+main() {
+  local workdir="${@:$#}"
+  local sar_out="$workdir/benchmark-io_sar_$(date +%Y-%m-%d-%H-%M-%S).out"
+
+  echo "$program: Output sar report in binary form to $sar_out"
+  echo "$program: Run benchmark-io"
+  echo ""
+
+  numactl -C 0 -- $@ &
+  local bench_pid=$!
+
+  sar -u -P 0 -o "$sar_out" > /dev/null &
+  local sar_pid $!
+
+  wait $bench_pid
+  kill -INT $sar_pid
+}
+
+
+if [[ $# -eq 0 ]]; then
+  usage
+  exit 1
+elif [[ "$1" = "-h" || "$1" = "--help" ]]; then
+  usage
+else
+  main $@
+fi
